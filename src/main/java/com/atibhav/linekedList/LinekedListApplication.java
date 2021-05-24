9c88f35f54a4273f90e1f16b6013ca1036e0ecbc
atsinha <atsinha@deloitte.com>
2021-05-24 17:16:06 +0530

    First Commit

---
+++ src/main/java/com/atibhav/linekedList/LinekedListApplication.java
@@ -0,0 +1,13 @@
+package com.atibhav.linekedList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinekedListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinekedListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atibhav/linekedList/LinekedListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atibhav.linekedList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinekedListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
